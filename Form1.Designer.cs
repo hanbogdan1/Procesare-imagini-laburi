e8820d2d9634a9f54781dc0f62275ab3246712e2
Ureniuc Bogdan <ureniucbogdan@gmail.com>
2018-04-22 14:25:46 +0300

    primele 2 laburi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optiuniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.incarcareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iesireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lab1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lab2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lab3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lAb4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lab5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lab6ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxVA = new System.Windows.Forms.TextBox();
+            this.textBoxVB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optiuniToolStripMenuItem,
+            this.lab1ToolStripMenuItem,
+            this.lab2ToolStripMenuItem,
+            this.lab3ToolStripMenuItem,
+            this.lAb4ToolStripMenuItem,
+            this.lab5ToolStripMenuItem,
+            this.lab6ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(982, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optiuniToolStripMenuItem
+            // 
+            this.optiuniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.incarcareToolStripMenuItem,
+            this.salvareToolStripMenuItem,
+            this.iesireToolStripMenuItem});
+            this.optiuniToolStripMenuItem.Name = "optiuniToolStripMenuItem";
+            this.optiuniToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.optiuniToolStripMenuItem.Text = "Optiuni";
+            // 
+            // incarcareToolStripMenuItem
+            // 
+            this.incarcareToolStripMenuItem.Name = "incarcareToolStripMenuItem";
+            this.incarcareToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.incarcareToolStripMenuItem.Text = "Incarcare";
+            this.incarcareToolStripMenuItem.Click += new System.EventHandler(this.incarcareToolStripMenuItem_Click);
+            // 
+            // salvareToolStripMenuItem
+            // 
+            this.salvareToolStripMenuItem.Name = "salvareToolStripMenuItem";
+            this.salvareToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.salvareToolStripMenuItem.Text = "Salvare";
+            this.salvareToolStripMenuItem.Click += new System.EventHandler(this.salvareToolStripMenuItem_Click);
+            // 
+            // iesireToolStripMenuItem
+            // 
+            this.iesireToolStripMenuItem.Name = "iesireToolStripMenuItem";
+            this.iesireToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.iesireToolStripMenuItem.Text = "Iesire";
+            // 
+            // lab1ToolStripMenuItem
+            // 
+            this.lab1ToolStripMenuItem.Name = "lab1ToolStripMenuItem";
+            this.lab1ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lab1ToolStripMenuItem.Text = "Lab1";
+            this.lab1ToolStripMenuItem.Click += new System.EventHandler(this.lab1ToolStripMenuItem_Click);
+            // 
+            // lab2ToolStripMenuItem
+            // 
+            this.lab2ToolStripMenuItem.Name = "lab2ToolStripMenuItem";
+            this.lab2ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lab2ToolStripMenuItem.Text = "Lab2";
+            this.lab2ToolStripMenuItem.Click += new System.EventHandler(this.lab2ToolStripMenuItem_Click);
+            // 
+            // lab3ToolStripMenuItem
+            // 
+            this.lab3ToolStripMenuItem.Name = "lab3ToolStripMenuItem";
+            this.lab3ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lab3ToolStripMenuItem.Text = "Lab3";
+            this.lab3ToolStripMenuItem.Click += new System.EventHandler(this.lab3ToolStripMenuItem_Click);
+            // 
+            // lAb4ToolStripMenuItem
+            // 
+            this.lAb4ToolStripMenuItem.Name = "lAb4ToolStripMenuItem";
+            this.lAb4ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lAb4ToolStripMenuItem.Text = "Lab4";
+            this.lAb4ToolStripMenuItem.Click += new System.EventHandler(this.lAb4ToolStripMenuItem_Click);
+            // 
+            // lab5ToolStripMenuItem
+            // 
+            this.lab5ToolStripMenuItem.Name = "lab5ToolStripMenuItem";
+            this.lab5ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lab5ToolStripMenuItem.Text = "Lab5";
+            // 
+            // lab6ToolStripMenuItem
+            // 
+            this.lab6ToolStripMenuItem.Name = "lab6ToolStripMenuItem";
+            this.lab6ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lab6ToolStripMenuItem.Text = "Lab6";
+            this.lab6ToolStripMenuItem.Click += new System.EventHandler(this.lab6ToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(314, 302);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(408, 33);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(315, 302);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(790, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(793, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(793, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "va:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(793, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "vb:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(859, 33);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(76, 20);
+            this.textBoxA.TabIndex = 7;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(859, 64);
+            this.textBoxB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(76, 20);
+            this.textBoxB.TabIndex = 8;
+            // 
+            // textBoxVA
+            // 
+            this.textBoxVA.Location = new System.Drawing.Point(859, 100);
+            this.textBoxVA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxVA.Name = "textBoxVA";
+            this.textBoxVA.Size = new System.Drawing.Size(76, 20);
+            this.textBoxVA.TabIndex = 9;
+            // 
+            // textBoxVB
+            // 
+            this.textBoxVB.Location = new System.Drawing.Point(859, 132);
+            this.textBoxVB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxVB.Name = "textBoxVB";
+            this.textBoxVB.Size = new System.Drawing.Size(76, 20);
+            this.textBoxVB.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(817, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 24);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Aplica!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(859, 278);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(982, 418);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxVB);
+            this.Controls.Add(this.textBoxVA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optiuniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem incarcareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iesireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lab1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lab2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lab3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lAb4ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxVA;
+        private System.Windows.Forms.TextBox textBoxVB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem lab5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lab6ToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,382 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace prelucrare_tema
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp;
+
         public Form1()
         {
             InitializeComponent();
+            lab3ToolStripMenuItem.Enabled = false;
+            lAb4ToolStripMenuItem.Enabled = false;
+            lab5ToolStripMenuItem.Enabled = false;
+            lab6ToolStripMenuItem.Enabled = false;
+        }
+
+        Bitmap bmp1;
+
+        int MedR(int i, int j)
+        {
+            int s = 0;
+            for (int ii = -1; ii <= 1; ii++)
+                for (int jj = -1; jj <= 1; jj++)
+                    s += bmp1.GetPixel(i + ii, j + jj).R;
+            return s / 9;
+        }
+
+        int MedG(int i, int j)
+        {
+            int s = 0;
+            for (int ii = -1; ii <= 1; ii++)
+                for (int jj = -1; jj <= 1; jj++)
+                    s += bmp1.GetPixel(i + ii, j + jj).G;
+            return s / 9;
+        }
+
+        int MedB(int i, int j)
+        {
+            int s = 0;
+            for (int ii = -1; ii <= 1; ii++)
+                for (int jj = -1; jj <= 1; jj++)
+                    s += bmp1.GetPixel(i + ii, j + jj).B;
+            return s / 9;
+        }
+
+        private void lAb4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            bmp1 = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            Console.WriteLine("-------" + bmp.Width);
+            label5.Text = bmp.Width + "";
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            //color of pixel
+            Color p;
+
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    int r = MedR(x, y);
+                    int g = MedG(x, y);
+                    int b = MedB(x, y);
+                    if (r > 255)
+                        r = 255;
+                    if (b > 255)
+                        b = 255;
+                    if (g > 255)
+                        g = 255;
+                    bmp1.SetPixel(x, y, Color.FromArgb(255, r, g, b));
+
+
+                }
+            }
+
+            pictureBox2.Image = bmp1;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            //save (write) sepia image
+
+        }
+
+        private void incarcareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            Console.WriteLine("-------" + bmp.Width);
+            label5.Text = bmp.Width + "";
+
+        }
+
+        private void salvareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bmp.Save("C:\\Users\\Bogdan\\Desktop\\nou.png");
+        }
+
+        private void lab1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //read image
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+
+            //load original image in picturebox1
+
+
+            //get image dimension
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            //color of pixel
+            Color p;
+
+            //sepia
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //get pixel value
+                    p = bmp.GetPixel(x, y);
+
+                    //extract pixel component ARGB
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    //calculate temp value
+                    int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+                    int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+                    int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
+
+                    //set new RGB value
+                    if (tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+
+                    //set the new RGB value in image pixel
+                    bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+
+            //load sepia image in picturebox2
+            pictureBox2.Image = bmp;
+        }
+
+        private void lab2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //read image
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+
+            //load original image in picturebox1
+
+
+            //get image dimension
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            //color of pixel
+            Color p;
+
+            //sepia
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //get pixel value
+                    p = bmp.GetPixel(x, y);
+
+                    //extract pixel component ARGB
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    //calculate temp value
+                    int tr = 255 - r;
+                    int tg = 255 - g;
+                    int tb = 255 - b;
+
+                    //set new RGB value
+                    if (tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+
+                    //set the new RGB value in image pixel
+                    bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+
+            pictureBox2.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+
+            int a = int.Parse(textBoxA.Text.ToString());
+            int b = int.Parse(textBoxB.Text.ToString());
+            int va = int.Parse(textBoxVA.Text.ToString());
+            int vb = int.Parse(textBoxVB.Text.ToString());
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+
+
+            //color of pixel
+            Color p;
+
+            //sepia
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //get pixel value
+                    p = bmp.GetPixel(x, y);
+                    int rC = p.R;
+                    int bC = p.B;
+                    int gC = p.G;
+                    int xx = (rC + bC + gC) / 3;
+                    int tr = 255, tb = 255, tg = 255;
+
+                    //extract pixel component ARGB
+                    if (xx <= a)
+                    {
+                        tr = rC * va / a;
+                        tb = bC * va / a;
+                        tg = gC * va / a;
+                    }
+
+                    if (a < xx && xx <= b)
+                    {
+                        tr = (int)(1.0 * (rC - a) / (b - a) * (vb - va) + va);
+                        tb = (int)(1.0 * (bC - a) / (b - a) * (vb - va) + va);
+                        tg = (int)(1.0 * (gC - a) / (b - a) * (vb - va) + va);
+                    }
+
+                    if (b < xx)
+                    {
+                        tr = (int)(1.0 * (rC - b) / (255 - b) * (255 - vb) + vb);
+                        tb = (int)(1.0 * (bC - b) / (255 - b) * (255 - vb) + vb);
+                        tg = (int)(1.0 * (gC - b) / (255 - b) * (255 - vb) + vb);
+                    }
+
+
+                    //set new RGB value
+                    if (tr > 255)
+                    {
+                        rC = 255;
+                    }
+                    else
+                    {
+                        rC = tr;
+                    }
+                    if (tr < 0)
+                        rC = 0;
+
+                    if (tg > 255)
+                    {
+                        gC = 255;
+                    }
+                    else
+                    {
+                        gC = tg;
+                    }
+
+                    if (tg < 0)
+                        gC = 0;
+
+                    if (tb > 255)
+                    {
+                        bC = 255;
+                    }
+                    else
+                    {
+                        bC = tb;
+                    }
+                    if (tb < 0)
+                        bC = 0;
+                    //set the new RGB value in image pixel
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, rC, gC, bC));
+
+                }
+
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void lab6ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            bmp1 = new Bitmap("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Bogdan\\Desktop\\double.jpg");
+            Console.WriteLine("-------" + bmp.Width);
+            label5.Text = bmp.Width + "";
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            //color of pixel
+            Color p;
+
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    bmp1.SetPixel(x, y, Color.FromArgb(255, 63, 31, 0));
+
+                }
+            }
+
+            pictureBox2.Image = bmp1;
+        }
+
+        private void lab3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
+
 }
+
